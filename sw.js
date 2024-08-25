264eff60754d8702c9fee661f79a6016c33088f5
Charlotte Harper <jasper@holton.io>
2024-08-25 14:11:01 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,15 +7,14 @@ var APP_PREFIX = 'qoshlli_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+var VERSION = 'version_01';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
-  `${GHPATH}/index/styles.css`,
-  `${GHPATH}/js/app.js`,
+  `${GHPATH}/main.js`,
   `${GHPATH}/qrcode.js`,
   `${GHPATH}/ccv.js`,
   `${GHPATH}/face.js`,
