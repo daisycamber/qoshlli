6ec7144ddc7888e41601a3e4254f1e8a4f018e75
Charlotte Harper <jasper@holton.io>
2024-08-25 16:54:13 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var APP_PREFIX = 'qoshlli_';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_02';
+var VERSION = 'version_03';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
@@ -20,6 +20,7 @@ var URLS = [
   `${GHPATH}/face.js`,
   `${GHPATH}/mirror.html`,
   `${GHPATH}/ringtone.mp3`,
+  `${GHPATH}/ringtone.wav`,
   `${GHPATH}/VK_logo.svg.png`,
   `${GHPATH}/pinterest.svg`,
   `${GHPATH}/tumblr-logo.svg`
